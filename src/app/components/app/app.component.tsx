6112f29775dff932bc7582f13f6a73ba21cf1783
Aniket Rathi <aniketrathi897@gmail.com>
2021-03-18 16:59:34 -0400

    added navbar

--- src/app/components/app/app.component.tsx
+++ src/app/components/app/app.component.tsx
@@ -1,39 +1,30 @@
+import * as React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Navbar, NavbarBrand } from "reactstrap";
 
-import * as React from 'react';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import { DIContext, getDependencies } from "@helpers";
 
-import {UsersComponent, HomeComponent} from '@components';
-import {PrivateRoute} from '@hoc';
-import {DIContext, getDependencies} from '@helpers';
-
-import './app.styles.css';
+import "./app.styles.css";
 
 const App = (): JSX.Element => {
+  const dependencies = React.useContext(DIContext);
+  const { translation } = dependencies;
   return (
     <DIContext.Provider value={getDependencies()}>
       <div className="center-wrap">
         <Router>
           <div>
-            <nav>
-              <img className="logo" src={require('@assets/images/logo.png')} alt="" />
-              <ul id="menu">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/users">Users</Link>
-                </li>
-              </ul>
-            </nav>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/" className="title">{translation.t("TODO")}</NavbarBrand>
+            </Navbar>
             <Switch>
-              <Route exact path="/" component={HomeComponent} />
-              <PrivateRoute exact path="/users" component={UsersComponent} />
+              {/* <Route exact path="/" component={HomeComponent} /> */}
             </Switch>
           </div>
         </Router>
       </div>
     </DIContext.Provider>
   );
-}
+};
 
 export default App;
